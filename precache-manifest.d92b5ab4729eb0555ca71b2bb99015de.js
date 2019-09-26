ed1d92bd064de9b0c56e9633954741cefa91a248
dansiecrown <dansiecrown@gmail.com>
2019-09-26 15:08:35 +0100

    Updates

---
+++ precache-manifest.d92b5ab4729eb0555ca71b2bb99015de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7abc3eeb201c4ee4129ccadc47972a81",
+    "url": "/robos/index.html"
+  },
+  {
+    "revision": "f094fc569e06bd06160a",
+    "url": "/robos/static/css/main.b2faff80.chunk.css"
+  },
+  {
+    "revision": "5319e232353c6359e09e",
+    "url": "/robos/static/js/2.dbc7d5f6.chunk.js"
+  },
+  {
+    "revision": "f094fc569e06bd06160a",
+    "url": "/robos/static/js/main.27ed44a5.chunk.js"
+  },
+  {
+    "revision": "de513c5705d9624a0585",
+    "url": "/robos/static/js/runtime-main.3aa724ed.js"
+  }
+]);
